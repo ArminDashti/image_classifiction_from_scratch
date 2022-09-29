4d120b45dc222e26f1477843d5f1ada35e7dbbfd
Armin <arminonline71@gmail.com>
2022-09-29 17:38:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt # for plotting
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torchvision
@@ -6,18 +6,14 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import os
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-batch_size = 4
-num_workers = 2
+transform = transforms.Compose([transforms.ToTensor(),
+      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,shuffle=True, num_workers=num_workers)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-
-classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+train_dataset_dir = 'C:\\Users\\Armin\\Desktop\\animals\\train'
+train_dataset = torchvision.datasets.ImageFolder(train_dataset_dir, transform=transform)
+train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True)
 
 class Net(nn.Module):
     def __init__(self):
@@ -25,27 +21,28 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5) 
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(44944, 1000)
+        self.fc2 = nn.Linear(1000, 500)
+        self.fc3 = nn.Linear(500, 100)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = x.view(-1, 44944)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
 net = Net()
+print(net)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 for epoch in range(2):
     running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
+    for i, data in enumerate(train_data_loader, 0):
         inputs, labels = data
         optimizer.zero_grad()
         outputs = net(inputs)
@@ -56,5 +53,4 @@ for epoch in range(2):
         if i % 2000 == 1999:
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-            
+            running_loss = 0.
